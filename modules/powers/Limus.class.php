181ff7e5b3bac3891c3ba3608a461ac76c8ebbc2
Tim <timothee.pecatta@ens-lyon.fr>
2020-05-19 01:19:27 +0200

    Limus should work.

--- modules/powers/Limus.class.php
+++ modules/powers/Limus.class.php
@@ -2,6 +2,8 @@
 
 class Limus extends Power
 {
+  public function isImplemented(){ return true; }
+
   public static function getId() {
     return LIMUS;
   }
@@ -16,7 +18,7 @@ class Limus extends Power
 
   public static function getText() {
     return [
-      clienttranslate("Opponent's Turn: Opponent Workers cannot build on spaces neighboring your Workers, unless building a dome to create a Complete Tower.")
+      clienttranslate("Opponent's Turn: Opponent Workers cannot build on spaces neighboring your Workers, unless building a dome. *** TODO: updated rule")
     ];
   }
 
@@ -29,10 +31,19 @@ class Limus extends Power
   }
 
   public static function isGoldenFleece() {
-    return true; 
+    return true;
   }
 
   /* * */
+  public function argOpponentBuild(&$arg)
+  {
+    $myWorkers = $this->game->board->getPlacedWorkers($this->playerId);
+    foreach($myWorkers as &$worker){
+      Utils::filterWorks($arg, function($space, $oppworker) use ($worker) {
+        // can build only a dome or at a non-neighbouring space
+        return $space['arg'] == 3 || !$this->game->board->isNeighbour($space, $worker, 'build');
+      });
+    }
+  }
 
 }
-  
