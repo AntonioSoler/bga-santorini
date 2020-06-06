84a119468a4f549b160abaa0b87e2fd6448ac660
quietmint <quietmint@users.noreply.github.com>
2020-06-06 13:19:02 -0400

    New text for Charybdis
    https://boardgamegeek.com/thread/2309022/article/35032762#35032762

--- modules/powers/Charybdis.class.php
+++ modules/powers/Charybdis.class.php
@@ -11,7 +11,7 @@ class Charybdis extends SantoriniPower
     $this->text  = [
       clienttranslate("Setup: Place 2 Whirlpool Tokens on your God Power card."),
       clienttranslate("End of Your Turn: You may place a Whirlpool Token from your God Power card on any unoccupied space on the board."),
-      clienttranslate("Any Time: Whirlpool Tokens built on or removed from the board are returned to your God Power card. A Worker cannot win by moving onto a whirlpool if the other whirlpool is on the board in an unoccupied space. Instead, the Worker is forced to the other whirlpool and may win as if it moved up to that space."),
+      clienttranslate("Any Time: If a Worker moves onto a Whirlpool and the other Whirlpool is on the board in an unoccupied space, it is forced to the other Whirlpool's space. In this case, the player cannot win by moving their Worker to the first Whirlpool's space but can win as if it had moved up to the second space. Whirlpool Tokens built on or removed are returned to your God Power card."),
       clienttranslate("[REVISED POWER]"),
     ];
     $this->playerCount = [2, 3, 4];
