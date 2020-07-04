7bc5220a0594aa48862b1c5ece32770f2ee742f2
quietmint <quietmint@users.noreply.github.com>
2020-07-04 00:34:25 -0400

    Implement Castor & Pollux

--- modules/powers/Castor.class.php
+++ modules/powers/Castor.class.php
@@ -15,7 +15,75 @@ class Castor extends SantoriniPower
     $this->playerCount = [2, 3, 4];
     $this->golden  = true;
     $this->orderAid = 13;
+
+    $this->implemented = true;
   }
 
   /* * */
+
+  public function argPlayerMove(&$arg)
+  {
+    $moves = $this->game->log->getLastMoves();
+    // Allow usual turn or skip
+    if (count($moves) <= 1) {
+      $arg['skippable'] = true;
+    }
+
+    // Otherwise, every worker must move once
+    $workersIds = array_map(function ($move) {
+      return $move['pieceId'];
+    }, $moves);
+    Utils::filterWorkersById($arg, $workersIds, false);
+  }
+
+  public function stateAfterMove()
+  {
+    $moves = count($this->game->log->getLastMoves());
+    if ($moves == 0) {
+      return null;
+    } else if ($moves == count($this->game->board->getPlacedActiveWorkers())) {
+      return 'endturn';
+    } else {
+      return 'moveAgain';
+    }
+  }
+
+  public function argPlayerBuild(&$arg)
+  {
+    $moves = $this->game->log->getLastMoves();
+    // Normal turn
+    if (count($moves) == 1) {
+      return;
+    }
+
+    // Oeverwise, every workers must build once
+    $builds = $this->game->log->getLastBuilds();
+    $workersIds = array_map(function ($build) {
+      return $build['pieceId'];
+    }, $builds);
+
+    $arg = $this->game->argPlayerWork('build');
+    Utils::filterWorkersById($arg, $workersIds, false);
+  }
+
+  public function stateAfterBuild()
+  {
+    $moves = count($this->game->log->getLastMoves());
+    // Normal turn
+    if ($moves == 1) {
+      return null;
+    }
+
+    $builds = count($this->game->log->getLastBuilds());
+    return $builds < count($this->game->board->getPlacedActiveWorkers()) ? 'buildAgain' : null;
+  }
+
+  public function endPlayerTurn()
+  {
+    $moves = count($this->game->log->getLastMoves());
+    if ($moves != 1) {
+      $stats = [[$this->playerId, 'usePower']];
+      $this->game->log->addAction('stats', $stats);
+    }
+  }
 }
