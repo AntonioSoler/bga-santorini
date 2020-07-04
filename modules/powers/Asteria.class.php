692fa71e0403a3023ee2d6b7491d6d90c0bf0e58
quietmint <quietmint@users.noreply.github.com>
2020-07-04 18:49:49 -0400

    Implement Asteria

--- modules/powers/Asteria.class.php
+++ modules/powers/Asteria.class.php
@@ -14,7 +14,44 @@ class Asteria extends SantoriniPower
     $this->playerCount = [2, 3, 4];
     $this->golden  = true;
     $this->orderAid = 45;
+
+    $this->implemented = true;
   }
 
   /* * */
+
+  public function hasMovedDown()
+  {
+    $moves = $this->game->log->getLastMoves($this->playerId);
+    return array_reduce($moves, function ($movedDown, $move) {
+      return $movedDown || $move['to']['z'] < $move['from']['z'];
+    }, false);
+  }
+
+  public function argPlayerBuild(&$arg)
+  {
+    $build = $this->game->log->getLastBuild($this->playerId);
+    // Normal build
+    if ($build == null) {
+      return null;
+    }
+
+    if ($this->hasMovedDown()) {
+      $worker = $this->game->board->getPiece($build['pieceId']);
+      $worker['works'] = $this->game->board->getAccessibleSpaces('build');
+      Utils::updateWorkerArgsBuildDome($worker, false);
+      $arg['workers'] = [$worker];
+      $arg['skippable'] = true;
+    }
+  }
+
+  public function stateAfterBuild()
+  {
+    $count = count($this->game->log->getLastBuilds($this->playerId));
+    if ($count > 1) {
+      $stats = [[$this->playerId, 'usePower']];
+      $this->game->log->addAction('stats', $stats);
+    }
+    return $count == 1 && $this->hasMovedDown() ? 'buildAgain' : null;
+  }
 }
