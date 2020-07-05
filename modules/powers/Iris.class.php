0f274d4cde23e393425330e3b373c4133c2b0113
quietmint <quietmint@users.noreply.github.com>
2020-07-05 09:09:01 -0400

    Implement Iris

--- modules/powers/Iris.class.php
+++ modules/powers/Iris.class.php
@@ -14,7 +14,41 @@ class Iris extends SantoriniPower
     $this->playerCount = [2, 3, 4];
     $this->golden  = true;
     $this->orderAid = 37;
+
+    $this->implemented = true;
   }
 
   /* * */
+
+  public function argPlayerMove(&$arg)
+  {
+    $accessibleSpaces = $this->game->board->getAccessibleSpaces('move');
+    $oppWorkers = $this->game->board->getPlacedOpponentWorkers();
+    $workers = $this->game->board->getPlacedActiveWorkers();
+    foreach ($workers as &$worker) {
+      $worker['works'] = [];
+      foreach ($oppWorkers as $worker2) {
+        // Only possible if workers are neighbors
+        if (!$this->game->board->isNeighbour($worker, $worker2)) {
+          continue;
+        }
+
+        // Check if space behind opponent is free
+        $space = $this->game->board->getSpaceBehind($worker, $worker2, $accessibleSpaces);
+        if (!is_null($space)) {
+          $worker['works'][] = $space;
+        }
+      }
+    }
+    Utils::mergeWorkers($arg, $workers);
+  }
+
+  public function afterPlayerMove($worker, $work)
+  {
+    $move = $this->game->log->getLastMove($this->playerId);
+    if (!$this->game->board->isNeighbour($move['from'], $move['to'])) {
+      $stats = [[$this->playerId, 'usePower']];
+      $this->game->log->addAction('stats', $stats);
+    }
+  }
 }
