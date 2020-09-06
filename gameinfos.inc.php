55d034a27b8fa27a0016ebd056c74df1c1c3ade8
quietmint <quietmint@users.noreply.github.com>
2020-09-06 12:55:19 -0400

    gameinfos: Remove not_recommend_player_number

--- gameinfos.inc.php
+++ gameinfos.inc.php
@@ -44,17 +44,16 @@ $gameinfos = array(
     'suggest_player_number' => 2,
 
     // Discourage players to play with these numbers of players. Must be null if there is no such advice.
-    'not_recommend_player_number' => array(3),
     // 'not_recommend_player_number' => array( 2, 3 ),      // <= example: this is not recommended to play this game with 2 or 3 players
 
     // Estimated game duration, in minutes (used only for the launch, afterward the real duration is computed)
     'estimated_duration' => 10,
 
     // Time in second add to a player when "giveExtraTime" is called (speed profile = fast)
-    'fast_additional_time' => 30,
+    'fast_additional_time' => 20,
 
     // Time in second add to a player when "giveExtraTime" is called (speed profile = medium)
-    'medium_additional_time' => 40,
+    'medium_additional_time' => 30,
 
     // Time in second add to a player when "giveExtraTime" is called (speed profile = slow)
     'slow_additional_time' => 50,
