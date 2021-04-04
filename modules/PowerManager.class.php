e3139f1b74795dacaed90a1d3f5980dda77ae248
quietmint <quietmint@users.noreply.github.com>
2021-04-04 12:05:09 -0400

    PowerManager resolve merge conflict (Hecate bans)

--- modules/PowerManager.class.php
+++ modules/PowerManager.class.php
@@ -182,16 +182,26 @@ class PowerManager extends APP_GameClass
     [ADONIS, TRITON], // multiple moves
 
     // Incomplete Hecate implementation: ban powers targetting opponent workers and other features to add
-    [HECATE, ADONIS],
     [HECATE, CHAOS], // Chaos should switch powers if a dome is built before an illegal action but not if building the dome was illegal
     [HECATE, CHARYBDIS], // not compatible with restarts if Hecate blocks the other whirlpool + during Hecate turn
     [HECATE, ERIS],
     [HECATE, IRIS], // can jump over secret workers but not build on the step / Hecate has to detect if this was legal + niche issue with restart implementation: may jump to a place where she cannot build, which is a possible move if Hecate is here (equivalent to pass the turn)
     [HECATE, NEMESIS],
     [HECATE, NYX], // too confusing for the moment
-    [HECATE, ODYSSEUS],
     [HECATE, SIREN],
     [HECATE, HYDRA], // should propose a higher level to spawn when <3 are of minimal height
+
+    // Hecate ban all heroes https://boardgamearena.com/bug?id=37708
+    [HECATE, ACHILLES],
+    [HECATE, ADONIS],
+    [HECATE, ATALANTA],
+    [HECATE, BELLEROPHON],
+    [HECATE, HERACLES],
+    [HECATE, JASON],
+    [HECATE, MEDEA],
+    [HECATE, ODYSSEUS],
+    [HECATE, POLYPHEMUS],
+    [HECATE, THESEUS],
   ];
 
 
